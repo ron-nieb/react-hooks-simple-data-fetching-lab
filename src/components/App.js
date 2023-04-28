055cc0af3c366c503691c331585c231ef7882493
Ronald Barasa <barasaogoro@gmail.com>
2023-04-28 16:10:50 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,19 @@
 // create your App component here
+import React, { useEffect, useState } from 'react'
+
+function App() {
+  const [dogBreed, setDogBreed] = useState(null)
+
+  useEffect(() => {
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then((response) => response.json())
+      .then((data) => {
+        setDogBreed(data.message)
+      })
+  }, [])
+
+  if (!dogBreed) return <p>Loading...</p>
+  return <img src={dogBreed} alt='A Random Dog' />
+}
+
+export default App
